00c2e205584ee725cd0ccdc5bc46abd595f72d8a
Kazim Kayhan <kazimmohammadi400@gmail.com>
2022-06-16 02:30:06 +0430

    Add about command

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -134,7 +134,7 @@ export const emacs = async (args?: string[]): Promise<string> => {
 };
 
 export const sudo = async (args?: string[]): Promise<string> => {
-  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); // ...I'm sorry
+  window.open('https://www.youtube.com/watch?v=ynUHE4OFg9E', '_blank');
   return `Permission denied: with little power comes... no responsibility? `;
 };
 
